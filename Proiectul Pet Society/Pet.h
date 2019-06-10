e85a6a8c6103f88bca07ba4fb8b681899625cc92
dragosefrem <dragosefrem@users.noreply.github.com>
2019-06-10 23:03:29 +0300

    Update Pet.h

--- Proiectul Pet Society/Pet.h
+++ Proiectul Pet Society/Pet.h
@@ -3,19 +3,12 @@
 
 #include "proiect.h"
 
-struct Pet
-{
-        char nume[20],rasa[20],tip[20];
-        int ID{},varstaLuni{};
-        bool esteCaine,estePisica,adoptat = 0,sterilizat = 0, vaccinat = 0;
-        int OK();
+struct Pet {
+  char nume[20], rasa[20], tip[20];
+  int ID{}, varstaLuni{};
+  bool esteCaine, estePisica, adoptat = 0, sterilizat = 0, vaccinat = 0;
+  int OK();
 
+} pet[100];
 
-
-
-}pet[100];
-
-
-
-
-#endif // PET_H
+#endif  // PET_H
